5b3425b7f0eaf3b2194e65d964b50778465947d7
Bo Chen <bo@liftoff.io>
2019-12-17 00:23:03 -0800

    Some terminology update + complete 2b

--- 2a/2a.py
+++ 2a/2a.py
@@ -4,20 +4,20 @@ import sys
 DEBUG = False
 
 class State:
-    program_counter: int = 0
+    instruction_counter: int = 0
     mem: list
-    def __init__(self, mem: list, program_counter: int=0):
+    def __init__(self, mem: list, instruction_counter: int=0):
         self.mem = mem
-        self.program_counter = program_counter
+        self.instruction_counter = instruction_counter
 
     def __str__(self):
-        out = f"program_counter: {self.program_counter}\nmem:\n" 
+        out = f"instruction_counter: {self.instruction_counter}\nmem:\n" 
         i = 0
         for n in range(0,10):
             out += f"\t{n}"
         out += f"\n-----------------------------------------------------------------------------------\n"
         while (i < len(self.mem)):
-            if (i == self.program_counter):
+            if (i == self.instruction_counter):
                 out += f"\t|{self.mem[i]}|"
             else:
                 out += f"\t{self.mem[i]}"
@@ -50,7 +50,7 @@ def decode_ins(ins):
 
 # Execute next instruction, return true if HALT
 def execute_next(state : State):  
-    ins = decode_ins(state.mem[state.program_counter : state.program_counter + 4])
+    ins = decode_ins(state.mem[state.instruction_counter : state.instruction_counter + 4])
     if DEBUG:
         print(ins)
     if ins["op"] == "ADD":
@@ -60,17 +60,13 @@ def execute_next(state : State):
     elif ins["op"] == "HALT":
         return True
 
-    state.program_counter += 4
+    state.instruction_counter += 4
     return False
 
-def main(file):
-    state = State(parse_input(file))
-    # kill me, just for 2a
-    state.mem[1] = 12
-    state.mem[2] = 2
-
-    print("START")
-    print(state)
+def execute_all(state : State):
+    if DEBUG:
+        print("START")
+        print(state)
 
     while True:
         should_exit = execute_next(state)
@@ -78,8 +74,21 @@ def main(file):
             print(state)
         if should_exit:
             break
-    print("END")
-    print(state)
+
+    if DEBUG:
+        print("END")
+        print(state)
+
+def main(file):
+    for n in range(0,100):
+        for v in range(0,100):
+            state = State(parse_input(file))
+            state.mem[1] = n
+            state.mem[2] = v
+            execute_all(state)
+            if (state.mem[0] == 19690720):
+                print(f"Noun: {n} Verb: {v} ")
+                break
 
 #main("sample3.txt")
 main("./input.txt")
